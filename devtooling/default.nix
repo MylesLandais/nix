e34eb12040e0e4c4103612b1eb9669c7a5be1eee
FKouhai <frandres00@gmail.com>
2025-10-26 03:58:45 +0100

    chore: properly configure git

--- devtooling/default.nix
+++ devtooling/default.nix
@@ -24,12 +24,12 @@
     elixir.enable = lib.mkDefault true;
     git.enable = lib.mkDefault true;
     gleam.enable = lib.mkDefault true;
-    go.enable = lib.mkDefault true;
-    kubernetes.enable = lib.mkDefault true;
-    lua.enable = lib.mkDefault true;
-    remmina.enable = lib.mkDefault true;
-    rust.enable = lib.mkDefault true;
-    tmux.enable = lib.mkDefault true;
+    go.enable = lib.mkDefault false;
+    kubernetes.enable = lib.mkDefault false;
+    lua.enable = lib.mkDefault false;
+    remmina.enable = lib.mkDefault false;
+    rust.enable = lib.mkDefault false;
+    tmux.enable = lib.mkDefault false;
     zed.enable = lib.mkDefault true;
   };
 }
