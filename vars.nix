94a5e292bd510bf83b1e8a5414be79e6f9c32321
FKouhai <frandres00@gmail.com>
2025-10-26 03:33:55 +0100

    chore: fix wallpaper definition
    create a derivation to package the nixos-artwork repo, and then leverage
    its output to set the wp

--- vars.nix
+++ vars.nix
@@ -1,10 +1,30 @@
 {
+  pkgs,
+}:
+let
+  wp = pkgs.stdenv.mkDerivation (rec {
+    pname = "wallpapers";
+    version = "v1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixos-artwork";
+      rev = "9d2cdedd73d64a068214482902adea3d02783ba8";
+      hash = "sha256-//4BiRF1W5W2rEbw6MupiyDOjvcveqGtYjJ1mZfck9U=";
+    };
+    buildInputs = [ ];
+    installPhase = ''
+      mkdir -p $out/share/wallpapers
+      cp -r ${src}/wallpapers/* $out/share/wallpapers
+    '';
+  });
+in
+{
   hostName = "cerberus-nix";
   username = "warby";
   userEmail = "warby@example.com";
 
   isDesktop = true;
-  wallpaper = "https://github.com/NixOS/nixos-artwork/raw/master/wallpapers/nix-wallpaper-nineish-catppuccin-mocha.png";
+  wallpaper = "${wp}/share/wallpapers/nix-wallpaper-nineish-catppuccin-mocha.png";
 
   mainMonitor = {
     name = "DP-1";
