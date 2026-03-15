9f792685caf6d0087c609a48e411168b5a86532b
FKouhai <frandres00@gmail.com>
2026-03-15 13:19:32 +0100

    chore(kraken): disable ollama

--- hosts/kraken/etc/nixos/ollama.nix
+++ hosts/kraken/etc/nixos/ollama.nix
@@ -8,11 +8,11 @@
 }:
 {
   services.ollama = {
-    enable = true;
+    enable = false;
     port = 11434;
     host = "0.0.0.0";
     openFirewall = true;
-    package = pkgs.ollama-cuda;
+    package = pkgs.ollama-rocm;
 
     loadModels = [
       "qwen2.5-coder:14b"
