f2fa1b10dc637bd63f39026501ba06b7d3c05218
warby <myles.landais@protonmail.com>
2026-05-21 11:40:31 -0500

    chore(colmena): register all current hosts in the hive
    Adds 94tl0m2, argus, lacie, and kali-vm entries alongside the existing
    95qmom2 node so colmena can address every host. Each entry mirrors the
    module set from its hosts/<name>/default.nix.

--- colmena.nix
+++ colmena.nix
@@ -12,6 +12,169 @@
     };
   };
 
+  "94tl0m2" =
+    { lib, ... }:
+    {
+      deployment = {
+        targetHost = "94tl0m2";
+        tags = [
+          "7050"
+          "staging"
+        ];
+      };
+      imports = [
+        inputs.self.nixosModules.tl0m2
+        inputs.self.nixosModules.tl0m2Hardware
+        inputs.self.nixosModules.tl0m2Postgres
+        inputs.self.nixosModules.tl0m2Seaweedfs
+        "${inputs.self}/modules/features/nix-config.nix"
+        "${inputs.self}/modules/features/fish-config.nix"
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            users.warby =
+              { pkgs, ... }:
+              {
+                home.username = lib.mkForce "warby";
+                home.homeDirectory = lib.mkForce "/home/warby";
+                home.stateVersion = "25.11";
+                programs.home-manager.enable = true;
+              };
+            extraSpecialArgs = {
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+          };
+        }
+      ];
+    };
+
+  "lacie" =
+    { lib, ... }:
+    {
+      deployment = {
+        targetHost = "lacie";
+        tags = [
+          "laptop"
+          "imaging"
+        ];
+      };
+      imports = [
+        inputs.self.nixosModules.lacie
+        inputs.self.nixosModules.lacieHardware
+        inputs.self.nixosModules.imaging
+        inputs.self.nixosModules.wifiProfiles
+        inputs.self.nixosModules.greeter
+        inputs.self.nixosModules.themeData
+        inputs.self.nixosModules.desktops
+        inputs.self.nixosModules.emulators
+        inputs.self.nixosModules.pentest
+        "${inputs.self}/modules/features/host-options.nix"
+        "${inputs.self}/modules/features/env-packages.nix"
+        "${inputs.self}/modules/features/nix-config.nix"
+        "${inputs.self}/modules/features/fish-config.nix"
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            sharedModules = [ inputs.agenix.homeManagerModules.age ];
+            users.warby =
+              { ... }:
+              {
+                imports = [ "${inputs.self}/modules/home.nix" ];
+                home.username = lib.mkForce "warby";
+                home.homeDirectory = lib.mkForce "/home/warby";
+                age.identityPaths = lib.mkForce [ "/home/warby/.ssh/age" ];
+              };
+            extraSpecialArgs = {
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+          };
+        }
+      ];
+    };
+
+  "argus" =
+    { lib, ... }:
+    {
+      deployment = {
+        targetHost = "argus";
+        tags = [
+          "headless"
+          "gpu"
+        ];
+      };
+      imports = [
+        inputs.self.nixosModules.argus
+        inputs.self.nixosModules.argusHardware
+        "${inputs.self}/modules/features/nix-config.nix"
+        "${inputs.self}/modules/features/fish-config.nix"
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            users.warby =
+              { pkgs, ... }:
+              {
+                home.username = lib.mkForce "warby";
+                home.homeDirectory = lib.mkForce "/home/warby";
+                home.stateVersion = "25.05";
+                programs.home-manager.enable = true;
+              };
+            extraSpecialArgs = {
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+          };
+        }
+      ];
+    };
+
+  "kali-vm" =
+    { lib, ... }:
+    {
+      deployment = {
+        targetHost = "kali-vm";
+        tags = [
+          "vm"
+          "kali"
+        ];
+      };
+      imports = [
+        inputs.self.nixosModules.kaliVm
+        inputs.self.nixosModules.themeData
+        inputs.self.nixosModules.desktops
+        "${inputs.self}/modules/features/host-options.nix"
+        "${inputs.self}/modules/features/env-packages.nix"
+        "${inputs.self}/modules/features/nix-config.nix"
+        "${inputs.self}/modules/features/fish-config.nix"
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            sharedModules = [ inputs.agenix.homeManagerModules.age ];
+            users.kali =
+              { ... }:
+              {
+                imports = [ "${inputs.self}/modules/home.nix" ];
+                home.username = lib.mkForce "kali";
+                home.homeDirectory = lib.mkForce "/home/kali";
+              };
+            extraSpecialArgs = {
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+          };
+        }
+      ];
+    };
+
   "95qmom2" =
     { lib, ... }:
     {
