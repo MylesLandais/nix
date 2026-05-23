b516cf927ac15b492c414464a136983624472e24
warby <myles.landais@protonmail.com>
2026-05-23 14:28:26 -0500

    chore(flake): add noctalia cachix binary cache

--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
       "https://nix-community.cachix.org/"
       "https://attic.xuyh0120.win/lantian"
       "https://cache.nixos.org/"
+      "https://noctalia.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
 
